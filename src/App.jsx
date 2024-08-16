00924a2e9fbd1a517e7028bd4acf99e5f76e6151
Golam Mustafa <golammustafa@gmail.com>
2024-08-16 19:41:04 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-4xl text-green-900 font-bold'>Search your product</h1>
+      
+    </>
+  )
+}
+
+export default App
